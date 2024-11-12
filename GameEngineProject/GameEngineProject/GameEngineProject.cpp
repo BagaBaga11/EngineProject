8e190f55d40fc2e62ff53973b1634fc782e4c70f
NatterMel <2221917@my.ipleiria.pt>
2024-11-12 22:42:39 +0000

    SDL Working

--- GameEngineProject/GameEngineProject/GameEngineProject.cpp
+++ GameEngineProject/GameEngineProject/GameEngineProject.cpp
@@ -13,6 +13,44 @@ void GameEngineProject::start(int sizeX, int sizeY)
     b2WorldId worldId = b2CreateWorld(&worldDef);
 
     SDL_Window* window = SDL_CreateWindow("SDL2", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, sizeY, sizeX, 0);
+<<<<<<< Updated upstream
     SDL_Delay(5000);
     SDL_DestroyWindow(window);
 }
+=======
+    SDL_Surface* windowSurface = SDL_GetWindowSurface(window);
+    SDL_Renderer* render = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+   SDL_Texture* backgroundTexture = LoadTexture(mylevel.GetBackground(), render);
+
+    while (isRunning)
+    {
+        SDL_UpdateWindowSurface(window);
+    }
+
+    SDL_DestroyWindow(window);
+}
+
+void Level::SetBackground(std::string imageName)
+{
+    background = imageName;
+}
+
+SDL_Texture* LoadTexture(std::string filePath, SDL_Renderer* renderTarget)
+{
+    SDL_Texture* texture = nullptr;
+    SDL_Surface* surface = SDL_LoadBMP(filePath.c_str());
+    if (surface == NULL)
+        std::cout << "MegaError" << std::endl;
+    else
+    {
+        SDL_SetColorKey(surface, SDL_TRUE, SDL_MapRGB(surface->format, 255, 0, 255));
+        texture = SDL_CreateTextureFromSurface(renderTarget, surface);
+        if (texture == NULL)
+            std::cout << "Error" << std::endl;
+    }
+
+    SDL_FreeSurface(surface);
+
+    return texture;
+}
+>>>>>>> Stashed changes
--- GameEngineProject/GameProject/GameProject.cpp
+++ GameEngineProject/GameProject/GameProject.cpp
@@ -2,10 +2,16 @@
 #include <iostream>
 #include "GameEngineProject.h"
 
-int main()
+int main(int argc, char* argv[])
 {
     GameEngineProject engine;
+<<<<<<< Updated upstream
 
+=======
+    Level mylevel;
+    mylevel.SetBackground("galaxy2.bmp");
+>>>>>>> Stashed changes
     engine.start(650,480);
+    return 0;
 }
 
